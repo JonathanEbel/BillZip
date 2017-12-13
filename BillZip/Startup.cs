b2ef7039d518416784450310b42ce27ef5a1d69b
Jonathan Ebel <jon@acvauctions.com>
2017-12-13 12:27:02 -0500

    Added swagger UI

--- BillZip/Startup.cs
+++ BillZip/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BillZip
 {
@@ -22,6 +23,12 @@ namespace BillZip
             services.AddMvc();
             services.AddEntityFrameworkNpgsql().AddDbContext<BuildingManagementContext>(opt => 
                 opt.UseNpgsql(Configuration.GetConnectionString("BuildingManagementConnection")));
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "BillZip API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +38,15 @@ namespace BillZip
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BillZip API V1");
+            });
 
             app.UseMvc();
         }
