462ab14e63ad844b8b8ddb08979c00bebbf0e9b3
Jonathan Ebel <jon@acvauctions.com>
2017-12-13 13:50:57 -0500

    Added debug flag for swagger so the swagger endpoint is not generated for Release....

--- BillZip/Startup.cs
+++ BillZip/Startup.cs
@@ -23,12 +23,13 @@ namespace BillZip
             services.AddMvc();
             services.AddEntityFrameworkNpgsql().AddDbContext<BuildingManagementContext>(opt => 
                 opt.UseNpgsql(Configuration.GetConnectionString("BuildingManagementConnection")));
-
+#if DEBUG 
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "BillZip API", Version = "v1" });
             });
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,16 +39,15 @@ namespace BillZip
             {
                 app.UseDeveloperExceptionPage();
             }
-
+#if DEBUG
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
-
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "BillZip API V1");
             });
-
+#endif
             app.UseMvc();
         }
     }
