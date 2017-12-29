0aa356797fb49de036f0dc3f5c210ee8deeb378f
Jonathan Ebel <Jonathan.ebel@gmail.com>
2017-12-29 13:58:59 -0500

    Policies setup has been laced in its own file for organization.

--- BillZip/Startup.cs
+++ BillZip/Startup.cs
@@ -12,8 +12,6 @@ using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.Threading.Tasks;
-using System.Reflection;
-using System.Linq;
 
 namespace BillZip
 {
@@ -62,34 +60,7 @@ namespace BillZip
 
                     });
 
-            
-            string authorizationPolicyNamespace = "BillZip.Policies";
-
-            var q = from t in Assembly.GetExecutingAssembly().GetTypes()
-                    where t.IsClass && t.Namespace == authorizationPolicyNamespace
-                    select t;
-            var list = q.ToList();
-            
-            foreach (var policy in list)
-            {
-                var props = policy.GetFields();
-                string policyName = "";
-                string requiredClaim = "";
-                string[] requiredValues = { };
-
-                policyName = (string)props.Where(prop => prop.Name == "PolicyName").First().GetRawConstantValue();
-                requiredClaim = (string)props.Where(prop => prop.Name == "RequireClaim").First().GetRawConstantValue();
-                requiredValues = (string[])props.Where(prop => prop.Name == "RequiredValues").First().GetValue(null);
-                services.AddAuthorization(options =>
-                {
-                    if (requiredValues.Length == 0)
-                        options.AddPolicy(policyName,
-                            p => p.RequireClaim(requiredClaim));
-                    else
-                        options.AddPolicy(policyName,
-                            p => p.RequireClaim(requiredClaim, requiredValues));
-                });
-            }
+            StartupHelpers.PolicySetup.BootstrapAuthorizationPolicies(services);
             
             services.AddMvc();
             services.AddEntityFrameworkNpgsql().AddDbContext<BuildingManagementContext>(opt => 
---
+++ BillZip/StartupHelpers/PolicySetup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
+
+namespace BillZip.StartupHelpers
+{
+    public static class PolicySetup
+    {
+        public static void BootstrapAuthorizationPolicies(IServiceCollection services)
+        {
+            string authorizationPolicyNamespace = "BillZip.Policies";
+
+            var q = from t in Assembly.GetExecutingAssembly().GetTypes()
+                    where t.IsClass && t.Namespace == authorizationPolicyNamespace
+                    select t;
+            var list = q.ToList();
+
+            foreach (var policy in list)
+            {
+                var props = policy.GetFields();
+                string policyName = "";
+                string requiredClaim = "";
+                string[] requiredValues = { };
+
+                policyName = (string)props.Where(prop => prop.Name == "PolicyName").First().GetRawConstantValue();
+                requiredClaim = (string)props.Where(prop => prop.Name == "RequireClaim").First().GetRawConstantValue();
+                requiredValues = (string[])props.Where(prop => prop.Name == "RequiredValues").First().GetValue(null);
+                services.AddAuthorization(options =>
+                {
+                    if (requiredValues.Length == 0)
+                        options.AddPolicy(policyName,
+                            p => p.RequireClaim(requiredClaim));
+                    else
+                        options.AddPolicy(policyName,
+                            p => p.RequireClaim(requiredClaim, requiredValues));
+                });
+            }
+        }
+    }
+}
