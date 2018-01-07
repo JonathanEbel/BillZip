6c909998ba2a14eff4fe0d398275c616789f81c6
Jonathan Ebel <Jonathan.ebel@gmail.com>
2018-01-06 21:35:33 -0500

    Returns all available claims to the server.

---
+++ BillZip/Controllers/ClaimsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BillZip.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/claims")]
+    [Authorize(Policy = Policies.Admin.PolicyName)]
+    public class ClaimsController: Controller
+    {
+        [HttpGet]
+        public Dictionary<string, string[]> Get()
+        {
+            string authorizationPolicyNamespace = "BillZip.Policies";
+
+            var q = from t in Assembly.GetExecutingAssembly().GetTypes()
+                    where t.IsClass && t.Namespace == authorizationPolicyNamespace
+                    select t;
+            var list = q.ToList();
+
+            var currentClaims = new Dictionary<string,string[]>();
+             
+            foreach (var policy in list)
+            {
+                // get all of the claims that are available from our policy class...
+                var props = policy.GetFields();
+                var claim = (string)props.Where(prop => prop.Name == "RequireClaim").First().GetRawConstantValue();
+                if (!currentClaims.Any(x => x.Key == claim))
+                    currentClaims.Add(claim, (string[])props.Where(prop => prop.Name == "RequiredValues").First().GetValue(null));
+            }
+
+            return currentClaims;
+        }
+    }
+}
